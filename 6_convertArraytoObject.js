689f8a22eb0cb85970d16ddec5213bd31dd9605f
SAnushree1 <anushree.satheesan@numentica-ui.com>
2025-09-08 09:41:27 +0530

    Update 6_convertArraytoObject.js

--- 6_convertArraytoObject.js
+++ 6_convertArraytoObject.js
@@ -2,7 +2,7 @@
 // toObject([["name", "Arun"], ["age", 39]]);
 // // Output: { name: "Arun", age: 39 }
 
-const inputArray = [["name", "Arun"], ["age", 39]];
+const inputArray = [["name", "Arun"], ["age", 39], ["name", "Aarik"]];
 
 function displayError(){
     console.log("Invalid input");
@@ -18,12 +18,24 @@ function convertArraytoObject(inputArray){
         if (inputArray[i].length === 2 && typeof(inputArray[i]) === "object"){
             let key = inputArray[i][0];
             let value = inputArray[i][1];
-            outputObject[key] = value;
+            // outputObject[key] = value;
+            let keyExists = false;
+            for (let existingKey in outputObject){
+                if (existingKey === key){
+                    keyExists = true;
+                    // console.log("Key already exists!!");
+                    break;
+                }   
+            }
+            if(!keyExists){
+                outputObject[key] = value;
+            }
         }else{
             continue;
         }
     }
     return outputObject;
 }
+
 
 console.log(convertArraytoObject(inputArray));
